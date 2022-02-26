806d50376f13c8fc456dbbbcb9348f23323dd7ee
Vito Chianese <vito.chianese@outlook.it>
2022-02-27 00:38:05 +0100

    HelloWorld.java

---
+++ HelloWorld.java
@@ -0,0 +1,31 @@
+package com;
+
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("ciao mondo");
+		
+		//int s = 20;
+		//System.out.println(s);
+		
+		//int s = 0;
+		//System.out.println("il valore di s è: "+s+" ti saluto mamma");
+		
+		//int s = 0;
+		//int r = 30;
+		//System.out.println("il valore di è: "+s+" il valore di r è: "+r);
+		
+		//System.out.print("ciao ");
+		//System.out.print("mondo");
+		
+		//System.out.print("ciao ");
+		//System.out.println("mondo ");
+		
+		//System.out.print("hello ");
+		//System.out.println("mondo ");
+		
+	}
+
+}

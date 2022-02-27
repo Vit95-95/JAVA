eb10b4f2d76cd15689f6afdf4d1cd146fe589267
Vito Chianese <vito.chianese@outlook.it>
2022-02-27 15:28:34 +0100

    EsempioOperatoriDiConfronto.java

---
+++ EsempioOperatoriDiConfronto.java
@@ -0,0 +1,22 @@
+package com;
+
+public class EsempioOperatoriDiConfronto {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		int x = 10;
+		int y = 4;
+		
+		if(x == 10) // x è uguale a 10
+			System.out.println("Condizione verificata");
+		if(x != 10) // x è diverso da 10
+			System.out.println("Condizione verificata");
+		if(x != y) // x è diverso da y
+			System.out.println("Condizione verificata");
+		if(x == y) // x è uguale a y
+			System.out.println("Condizione verificata");
+		
+	}
+
+}
